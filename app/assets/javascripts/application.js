a1c6c5f2500b7aeee806521de621a04b0ebe5134
Hannah Lehman <hjlehman@gmail.com>
2015-03-10 09:45:26 -0500

    Add google map api.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function (){
+
+    function initialize() {
+      var mapOptions = {
+      center: { lat:29.929266, lng:-90.084196},
+      zoom: 8
+      }; 
+        
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);    
+    }
+      
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
